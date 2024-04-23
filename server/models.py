fdb2a978c97bd970b233b256a732914d689e068f
Gonzo2510 <Jagonz@live.com>
2024-04-22 19:44:08 -0500

    models complete

--- server/models.py
+++ server/models.py
@@ -7,20 +7,25 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    _password_hash = db.Column(db.String, nullable=False)
+
+    serialize_rules = ("-recipes.user", "-password_hash",)
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique = True, nullable = False)
+    _password_hash = db.Column(db.String)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
+    # Relationship
+    recipes = db.relationship('Recipe', backref = "user")
+
     @hybrid_property
     def password_hash(self):
-        return self._password_hash
+        raise AttributeError("Password hashes may not be viewed.")
     
     @password_hash.setter
     def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(
-             password.encode('utf-8'))
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
         self._password_hash = password_hash.decode('utf-8')
 
     def authenticate(self, password):
@@ -31,14 +36,13 @@ class User(db.Model, SerializerMixin):
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    instructions = db.Column(db.String)
+    __table_args__ = (
+        db.CheckConstraint("length(instructions) >= 50"),
+    )
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable = False)
+    instructions = db.Column(db.String, nullable = False)
     minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     
-    @validates('instructions')
-    def validate_instructions(self, key, instructions):
-        if len(instructions) < 50:
-            raise ValueError('instructions must be longer than 50 characters')
-        else:
-            return instructions
